d9e76b06bb52a8a08f6bf2fc479d18ad95fbca81
jsantos93 <jcas@cesar.school>
2018-10-04 17:32:05 -0300

    Add First interraction

--- jcas.c
+++ jcas.c
@@ -2,5 +2,17 @@
 
 int main()
 {
+    int n;
+    char heap;
+
+    scanf("%d", &n);
+
+    for (int *i = 0; i < (n - 1); i++)
+    {
+        scanf(" %c", &heap[i]);
+    }
+
+    printf("fon");
+
     return 0;
 }
